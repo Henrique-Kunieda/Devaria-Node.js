2d372cd0c9466e420899d6714545b2c7138ad10f
Henrique-Kunieda <henriquekunieda.hk@gmail.com>
2024-05-21 19:56:24 -0300

    Create app.js
    ARRAY E FUNÇÕES

---
+++ arrays-exceçoes/app.js
@@ -0,0 +1,30 @@
+try{
+    const listaDeProdutosDisponiveis = [
+        'Pão',
+        'Leite',
+        'cafe',
+        'Sabonete',
+        'Laranja',
+        'Macarrão',
+        'Detergente',
+    ];
+    
+    const listaDeArgumentos = process.argv.slice(2)
+    
+    const  listaDeProdutosSolicitadosDisponiveis =  listaDeProdutosDisponiveis.filter(produto =>{
+        return listaDeArgumentos.find(argumento => argumento === produto);
+    })
+    
+    listaDeProdutosSolicitadosDisponiveis.forEach( produto => console.log( `Este produto nos temos : ${produto}`))
+    
+    listaDeProdutosSolicitadosNaoDisponiveis = listaDeArgumentos.filter(argumento =>{
+        return !listaDeProdutosDisponiveis.find(produto => produto === argumento)
+    })
+    listaDeProdutosSolicitadosNaoDisponiveis.forEach( argumento => console.log(`Este produto nos não temos : ${argumento}`))
+    
+     listaDeProdutosDisponiveis.sort()
+    listaDeProdutosDisponiveis.forEach( produto => console.log(`Este produto está disponível: ${produto}`));
+}catch(e){
+    console.log('Não foi possivel executar o pedido da compra')
+}
+
